5dde402d59ead1a96dc1f191cc733e3022556ae4
Louis Cleen <40666043+louiscleen@users.noreply.github.com>
2026-04-25 12:36:10 +0200

    Move solar constant to the config file and correct comments for equinox and perihelion days in constants.py

--- src/ebm1d/constants.py
+++ src/ebm1d/constants.py
@@ -1,10 +1,9 @@
 """Physical constants used in the 1D EBM."""
 
-SOLAR_CONSTANT = 1361.0      # Total solar irradiance [W m^-2]
 EARTH_RADIUS = 6371e3        # Earth radius [m]
 EARTH_ECCENTRICITY = 0.0167  # Earth's orbital eccentricity [dimensionless]
 P_CO2_0 = 280.0              # Reference CO2 concentration (pre-industrial) [ppm]
 SIN_EPSILON = 0.39795        # Sine of Earth's obliquity (tilt) [dimensionless]
-YEAR_IN_DAYS = 365        # Number of days in a year [days]
-SPRING_EQUINOX_DAY = 80.0    # Approximate day of the spring equinox (March 21) [day of year]
-PERIHELION_DAY = 3.0         # Approximate day of perihelion (January 3) [day of year]
+YEAR_IN_DAYS = 365           # Number of days in a year [days]
+VERNAL_EQUINOX_DAY = 80.0    # Approximate day of the vernal (spring) equinox in the Northern Hemisphere (~ March 21) [day of year]
+PERIHELION_DAY = 3.0         # Approximate day of perihelion (~ January 3) in the Northern Hemisphere [day of year]
